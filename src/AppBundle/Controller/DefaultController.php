8fd44591b40c81287669f7926189b98da06fa7d9
tr <tr@ENVY>
2016-03-31 21:03:12 +0800

    Start to write about layouts

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -14,8 +14,6 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
-        ]);
+        return $this->render('AppBundle:default:layout.html.twig');
     }
 }
